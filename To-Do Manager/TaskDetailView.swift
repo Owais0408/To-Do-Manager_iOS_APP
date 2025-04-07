965c97958f65d85800b35e6b60e8a40023637273
tushartyagi883 <tushartyagi883@gmail.com>
2025-04-07 00:26:13 -0400

    first commit

---
+++ To-Do Manager/TaskDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  TaskDetailView.swift
+//  To-Do Manager
+//
+//  Created by Owais Khan on 2025-04-06.
+//
+
+import Foundation
---
+++ To-Do Manager/TaskStorage.swift
@@ -0,0 +1,8 @@
+//
+//  TaskStorage.swift
+//  To-Do Manager
+//
+//  Created by Owais Khan on 2025-04-06.
+//
+
+import Foundation
